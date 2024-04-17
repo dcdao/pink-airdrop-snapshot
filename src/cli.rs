dad83f996b73ae38289e5b09660f383f0dbed196
Xavier Lau <xavier@inv.cafe>
2024-04-17 19:07:00 +0800

    Release `0.3.0`

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,6 @@
 // crates.io
 use anyhow::Result;
 use clap::Parser;
-use futures::future;
 // pas
 use crate::snapshot::Pas;
 
@@ -24,48 +23,7 @@ pub struct Cli {
 }
 impl Cli {
 	pub async fn run(&self) -> Result<()> {
-		let pas = Pas::new(&self.ws).await?;
-
-		for f in future::join_all([
-			// RING and deposit.
-			// 2023-10-11 00:01:06 (+UTC)
-			// https://darwinia.subscan.io/block/1186226
-			pas.snap(
-				"0x2562247315cc789376da2049c6e792f689387781fd1249374ce238760547b578",
-				true,
-				true,
-			),
-			// KTON.
-			// 2023-09-26 00:00:06 (+UTC)
-			// https://darwinia.subscan.io/block/1080104
-			pas.snap(
-				"0x6768a1fe876999de62a0120f50190f50b126fdcf0d0bee8a83b3b692f5e47a48",
-				false,
-				true,
-			),
-			// RING and deposit.
-			// 2024-01-23 16:51:00 (+UTC)
-			// https://darwinia.subscan.io/block/1910929
-			pas.snap(
-				"0x7bcab5906d0ceceec70874f7eb12093ee42122b5e4d6c8dfc613084186d5f282",
-				true,
-				false,
-			),
-			// KTON.
-			// 2024-01-23 16:51:00 (+UTC)
-			// https://darwinia.subscan.io/block/1910929
-			pas.snap(
-				"0x7bcab5906d0ceceec70874f7eb12093ee42122b5e4d6c8dfc613084186d5f282",
-				false,
-				false,
-			),
-		])
-		.await
-		{
-			f?;
-		}
-
-		pas.save()?;
+		Pas::new(&self.ws).await?.snap().await?.merge().save()?;
 
 		Ok(())
 	}
--- src/snapshot.rs
+++ src/snapshot.rs
@@ -1,10 +1,12 @@
 // std
-use std::{collections::HashMap, fs::File, sync::Arc};
+use std::{
+	collections::{HashMap, HashSet},
+	fs::File,
+};
 // crates.io
 use anyhow::Result;
 use chrono::Utc;
 use csv::Writer;
-use futures::{future, FutureExt};
 use parity_scale_codec::Decode;
 use serde::Serialize;
 use subxt::{
@@ -13,46 +15,59 @@ use subxt::{
 	storage::{address::Yes, StorageAddress},
 	OnlineClient, PolkadotConfig,
 };
-use tokio::sync::{Mutex, MutexGuard};
-
-type Map = HashMap<[u8; 20], Data>;
 
 const RING_THRESHOLD: u128 = 1_000_000_000_000_000_000;
 const KTON_THRESHOLD: u128 = 10_000_000_000_000_000;
 
 pub struct Pas {
 	api: OnlineClient<PolkadotConfig>,
-	map: Arc<Mutex<Map>>,
+	ring_eligible_list: HashSet<String>,
+	kton_eligible_list: HashSet<String>,
+	deposit_eligible_list: HashSet<String>,
+	ring_kton_holders_detail: Vec<(String, u128, u128)>,
+	deposit_holders_detail: Vec<(String, u128)>,
+	data: HashMap<String, FinalData>,
 }
 impl Pas {
 	pub async fn new(ws: &str) -> Result<Self> {
 		let api = <OnlineClient<PolkadotConfig>>::from_url(ws).await?;
 
-		Ok(Self { api, map: Arc::new(Mutex::new(Map::new())) })
+		Ok(Self {
+			api,
+			ring_eligible_list: HashSet::new(),
+			kton_eligible_list: HashSet::new(),
+			deposit_eligible_list: HashSet::new(),
+			ring_kton_holders_detail: Vec::new(),
+			deposit_holders_detail: Vec::new(),
+			data: HashMap::new(),
+		})
 	}
 
-	async fn iter_storage_with<F, A, S>(
+	async fn iter_storage_with<A, C, F, S>(
 		&self,
-		map: Arc<Mutex<Map>>,
-		at: H256,
+		at: &str,
 		address: A,
+		collection: &mut C,
 		f: F,
 	) -> Result<()>
 	where
-		F: Fn(MutexGuard<Map>, [u8; 20], S),
 		A: 'static + StorageAddress<IsIterable = Yes, Target = DecodedValueThunk>,
+		F: Fn(&mut C, String, S),
 		S: Decode,
 	{
+		let at = array_bytes::hex_n_into_unchecked::<_, H256, 32>(at);
 		let mut storage = self.api.storage().at(at).iter(address).await?;
 
 		while let Some(r) = storage.next().await {
 			match r {
 				Ok((k, v)) => {
-					let who = array_bytes::slice2array_unchecked::<_, 20>(&k[k.len() - 20..]);
-					let s = S::decode(&mut &*v.into_encoded()).unwrap();
-					let map = map.lock().await;
+					let k = array_bytes::bytes2hex(
+						"0x",
+						array_bytes::slice2array_unchecked::<_, 20>(&k[k.len() - 20..]),
+					);
+					let v = S::decode(&mut &*v.into_encoded()).unwrap();
 
-					f(map, who, s);
+					f(collection, k, v);
 				},
 				Err(e) => println!("Error: {e:?}"),
 			}
@@ -61,110 +76,218 @@ impl Pas {
 		Ok(())
 	}
 
-	pub async fn snap(
-		&self,
-		at: &str,
-		// Snapshot target.
-		// `true` for RING and `false` for KTON.
-		ring: bool,
-		// Check eligibility won't calculate the staked amount.
-		check_eligibility: bool,
-	) -> Result<()> {
-		let at = array_bytes::hex_n_into_unchecked::<_, H256, 32>(at);
-		let f1 = self
-			.iter_storage_with(
-				self.map.clone(),
-				at,
-				dynamic::storage("DarwiniaStaking", "Ledgers", <Vec<()>>::new()),
-				|mut map, who, ledger: Ledger| {
-					if ring {
-						// Check if eligibility needs to be checked and if it passes the threshold,
-						if (check_eligibility && ledger.staked_ring >= RING_THRESHOLD)
-							// or if it just needs to be greater than 0.
-							|| (!check_eligibility && ledger.staked_ring > 0)
-						{
-							// If checking eligibility, simply insert a record; otherwise, add the
-							// staked amount.
-							let ring = if check_eligibility { 0 } else { ledger.staked_ring };
-
-							map.entry(who).and_modify(|d| d.ring += ring).or_insert_with(|| Data {
-								who: array_bytes::bytes2hex("0x", who),
-								ring,
-								kton: 0,
-							});
-						}
-					} else {
-						// Check if eligibility needs to be checked and if it passes the threshold,
-						if (check_eligibility && ledger.staked_kton >= KTON_THRESHOLD)
-							// or if it just needs to be greater than 0.
-							|| (!check_eligibility && ledger.staked_kton > 0)
-						{
-							// If checking eligibility, simply insert a record; otherwise, add the
-							// staked amount.
-							let kton = if check_eligibility { 0 } else { ledger.staked_kton };
-
-							map.entry(who).and_modify(|d| d.kton += kton).or_insert_with(|| Data {
-								who: array_bytes::bytes2hex("0x", who),
-								ring: 0,
-								kton,
-							});
-						}
-					}
-				},
-			)
-			.boxed();
-		let f2 = if ring {
+	pub async fn snap(mut self) -> Result<Self> {
+		let mut ring_eligible_list = HashSet::new();
+		let mut kton_eligible_list = HashSet::new();
+		let mut deposit_eligible_list = HashSet::new();
+		let mut ring_kton_holders_detail = Vec::new();
+		let mut deposit_holders_detail = Vec::new();
+		let (f1, f2, f3, f4, f5) = tokio::join!(
+			// ---
+			// 2023-10-11 00:01:06 (+UTC)
+			// https://darwinia.subscan.io/block/1186226
+			// ---
+			//
+			// A list for RING at this moment.
 			self.iter_storage_with(
-				self.map.clone(),
-				at,
+				"0x2562247315cc789376da2049c6e792f689387781fd1249374ce238760547b578",
+				dynamic::storage("DarwiniaStaking", "Ledgers", <Vec<()>>::new()),
+				&mut ring_eligible_list,
+				Box::new(|c: &mut HashSet<_>, k, v: Ledger| {
+					if v.staked_ring >= RING_THRESHOLD {
+						c.insert(k);
+					}
+				}),
+			),
+			// A list for Deposit at this moment.
+			self.iter_storage_with(
+				"0x2562247315cc789376da2049c6e792f689387781fd1249374ce238760547b578",
 				dynamic::storage("Deposit", "Deposits", <Vec<()>>::new()),
-				|mut map, who, deposits: Vec<Deposit>| {
-					// If checking eligibility, simply insert a record; otherwise, add the
-					// staked amount.
-					// No threshold requirement for deposit.
-					let ring = if check_eligibility {
-						0
-					} else {
-						deposits.iter().fold(0, |acc, d| acc + d.value)
-					};
+				&mut deposit_eligible_list,
+				Box::new(|c: &mut HashSet<_>, k, v: Deposit| {
+					if v.value > 0 {
+						c.insert(k);
+					}
+				}),
+			),
+			// ---
+			// 2023-09-26 00:00:06 (+UTC)
+			// https://darwinia.subscan.io/block/1080104
+			// ---
+			//
+			// A list for KTON at this moment.
+			self.iter_storage_with(
+				"0x6768a1fe876999de62a0120f50190f50b126fdcf0d0bee8a83b3b692f5e47a48",
+				dynamic::storage("DarwiniaStaking", "Ledgers", <Vec<()>>::new()),
+				&mut kton_eligible_list,
+				Box::new(|c: &mut HashSet<_>, k, v: Ledger| {
+					if v.staked_kton >= KTON_THRESHOLD {
+						c.insert(k);
+					}
+				}),
+			),
+			// ---
+			// 2024-01-23 16:51:00 (+UTC)
+			// https://darwinia.subscan.io/block/1910929
+			// ---
+			//
+			// A list for RING and KTON at this moment.
+			self.iter_storage_with(
+				"0x7bcab5906d0ceceec70874f7eb12093ee42122b5e4d6c8dfc613084186d5f282",
+				dynamic::storage("DarwiniaStaking", "Ledgers", <Vec<()>>::new()),
+				&mut ring_kton_holders_detail,
+				Box::new(|c: &mut Vec<_>, k, v: Ledger| {
+					c.push((k, v.staked_ring, v.staked_kton));
+				}),
+			),
+			// A list for Deposit at this moment.
+			self.iter_storage_with(
+				"0x7bcab5906d0ceceec70874f7eb12093ee42122b5e4d6c8dfc613084186d5f282",
+				dynamic::storage("Deposit", "Deposits", <Vec<()>>::new()),
+				&mut deposit_holders_detail,
+				Box::new(|c: &mut Vec<_>, k, v: Deposit| {
+					c.push((k, v.value));
+				}),
+			),
+		);
 
-					map.entry(who).and_modify(|d| d.ring += ring).or_insert_with(|| Data {
-						who: array_bytes::bytes2hex("0x", who),
-						ring,
-						kton: 0,
-					});
-				},
-			)
-			.boxed()
-		} else {
-			future::ready(Ok(())).boxed()
-		};
+		f1?;
+		f2?;
+		f3?;
+		f4?;
+		f5?;
 
-		for r in future::join_all([f1, f2]).await {
-			r?;
-		}
+		self.ring_eligible_list = ring_eligible_list;
+		self.kton_eligible_list = kton_eligible_list;
+		self.deposit_eligible_list = deposit_eligible_list;
+		self.ring_kton_holders_detail = ring_kton_holders_detail;
+		self.deposit_holders_detail = deposit_holders_detail;
 
-		Ok(())
+		Ok(self)
 	}
 
-	pub fn save(self) -> Result<()> {
-		let f = File::create(format!("eligible-list-{}.csv", Utc::now().format("%F-%H-%M-%S")))?;
-		let mut wtr = Writer::from_writer(f);
-
-		for (_, d) in Arc::try_unwrap(self.map).unwrap().into_inner() {
-			if d.ring > 0 || d.kton > 0 {
-				wtr.serialize(d)?;
+	pub fn merge(mut self) -> Self {
+		for &(ref who, ring, kton) in &self.ring_kton_holders_detail {
+			if self.ring_eligible_list.contains(who) {
+				if let Some(d) = self.data.get_mut(who) {
+					d.ring += ring;
+				} else {
+					self.data.insert(who.into(), FinalData { who: who.into(), ring, kton: 0 });
+				}
+			}
+			if self.kton_eligible_list.contains(who) {
+				if let Some(d) = self.data.get_mut(who) {
+					d.kton += kton;
+				} else {
+					self.data.insert(who.into(), FinalData { who: who.into(), ring: 0, kton });
+				}
+			}
+		}
+		for &(ref who, ring) in &self.deposit_holders_detail {
+			if self.deposit_eligible_list.contains(who) {
+				if let Some(d) = self.data.get_mut(who) {
+					d.ring += ring;
+				} else {
+					self.data.insert(who.into(), FinalData { who: who.into(), ring, kton: 0 });
+				}
 			}
 		}
 
-		wtr.flush()?;
+		self
+	}
+
+	pub fn save(self) -> Result<()> {
+		let now = Utc::now().format("%F-%H-%M-%S");
+
+		{
+			let f = File::create(format!("snapshot-ring-eligible-list-{now}.csv"))?;
+			let mut wtr = Writer::from_writer(f);
+
+			for who in self.ring_eligible_list {
+				wtr.serialize(UserData { who })?;
+			}
+
+			wtr.flush()?;
+		}
+
+		{
+			let f = File::create(format!("snapshot-kton-eligible-list{now}.csv"))?;
+			let mut wtr = Writer::from_writer(f);
+
+			for who in self.kton_eligible_list {
+				wtr.serialize(UserData { who })?;
+			}
+
+			wtr.flush()?;
+		}
+
+		{
+			let f = File::create(format!("snapshot-deposit-eligible-list-{now}.csv"))?;
+			let mut wtr = Writer::from_writer(f);
+
+			for who in self.deposit_eligible_list {
+				wtr.serialize(UserData { who })?;
+			}
+
+			wtr.flush()?;
+		}
+
+		{
+			let f1 = File::create(format!("snapshot-ring-holders-detail-{now}.csv"))?;
+			let f2 = File::create(format!("snapshot-kton-holders-detail-{now}.csv"))?;
+			let mut wtr1 = Writer::from_writer(f1);
+			let mut wtr2 = Writer::from_writer(f2);
+
+			for (who, ring, kton) in self.ring_kton_holders_detail {
+				wtr1.serialize(BasicData { who: who.clone(), amount: ring })?;
+				wtr2.serialize(BasicData { who, amount: kton })?;
+			}
+
+			wtr1.flush()?;
+			wtr2.flush()?;
+		}
+
+		{
+			let f = File::create(format!("snapshot-deposit-holders-detail-{now}.csv"))?;
+			let mut wtr = Writer::from_writer(f);
+
+			for (who, ring) in self.deposit_holders_detail {
+				wtr.serialize(BasicData { who, amount: ring })?;
+			}
+
+			wtr.flush()?;
+		}
+
+		{
+			let f = File::create(format!("eligible-list-{now}.csv"))?;
+			let mut wtr = Writer::from_writer(f);
+
+			for (_, d) in self.data {
+				if d.ring > 0 || d.kton > 0 {
+					wtr.serialize(d)?;
+				}
+			}
+
+			wtr.flush()?;
+		}
 
 		Ok(())
 	}
 }
 
-#[derive(Debug, Decode, Serialize)]
-struct Data {
+#[derive(Debug, Default, Decode, Serialize)]
+struct UserData {
+	who: String,
+}
+
+#[derive(Debug, Default, Decode, Serialize)]
+struct BasicData {
+	who: String,
+	amount: u128,
+}
+
+#[derive(Debug, Default, Decode, Serialize)]
+struct FinalData {
 	who: String,
 	ring: u128,
 	kton: u128,
